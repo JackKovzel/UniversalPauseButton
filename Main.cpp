6f092fdb2d9eff91b3ead3bc893948713830f624
Jack Kovzel <JackKovzel@gmail.com>
2015-06-14 20:25:39 +0300

    Pause when xbox guide button was pressed

--- Main.cpp
+++ Main.cpp
@@ -167,6 +167,41 @@ Default:
 // Entry point.
 int CALLBACK WinMain(_In_ HINSTANCE Instance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
+	struct ControllerStruct
+	{
+		unsigned long eventCount; //event counter, increases with every controller event,
+		//but for some reason not by one.
+		unsigned short up : 1, down : 1, left : 1, right : 1, start : 1, back : 1, l3 : 1, r3 : 1,
+		lButton : 1, rButton : 1, guideButton : 1, unknown : 1, aButton : 1,
+			  bButton : 1, xButton : 1, yButton : 1; // button state bitfield
+		unsigned char lTrigger;  //Left Trigger
+		unsigned char rTrigger;  //Right Trigger
+		short lJoyY;  //Left Joystick Y
+		short lJoyx;  //Left Joystick X
+		short rJoyY;  //Right Joystick Y 
+		short rJoyX;  //Right Joystick X
+	};
+
+	//First create an HINSTANCE of the xinput1_3.dll.  Probably should use system variables to find it
+	//but whatever.
+	//HINSTANCE hGetProcIDDLL = LoadLibrary("C:/Windows/System32/xinput1_3.dll");  //In Visual Studio replace this
+	HINSTANCE hGetProcIDDLL = LoadLibrary(L"xinput1_3.dll");  //With this
+
+	//Get the address of ordinal 100.
+	FARPROC lpfnGetProcessID = GetProcAddress(HMODULE(hGetProcIDDLL), (LPCSTR)100);
+
+	//typedef the function. It takes an int and a pointer to a ControllerStruct and returns an error code
+	//as an int.  it's 0 for no error and 1167 for "controller not present".  presumably there are others
+	//but I never saw them.  It won't cause a crash on error, it just won't update the data.
+	typedef int(__stdcall * pICFUNC)(int, ControllerStruct &);
+
+	//Assign it to getControllerData for easier use
+	pICFUNC getControllerData;
+	getControllerData = pICFUNC(lpfnGetProcessID);
+
+	//Create in an instance of the ControllerStruct
+	ControllerStruct buttons;
+
 	G_Mutex = CreateMutex(NULL, FALSE, L"UniversalPauseButton");
 	if (GetLastError() == ERROR_ALREADY_EXISTS)
 	{
@@ -251,9 +286,9 @@ int CALLBACK WinMain(_In_ HINSTANCE Instance, _In_opt_ HINSTANCE, _In_ LPSTR, _I
 			DispatchMessage(&SysTrayWindowMessage);
 		}
 
-		int PauseKeyIsDown = GetAsyncKeyState(PauseKey);
+		getControllerData(0, buttons);
 
-		if (PauseKeyIsDown && !PauseKeyWasDown)
+		if (buttons.guideButton && !PauseKeyWasDown)
 		{
 			HWND ForegroundWindow = GetForegroundWindow();
 			if (!ForegroundWindow)
@@ -353,7 +388,7 @@ int CALLBACK WinMain(_In_ HINSTANCE Instance, _In_opt_ HINSTANCE, _In_ LPSTR, _I
 			}
 		}
 		
-		PauseKeyWasDown = PauseKeyIsDown;
+		PauseKeyWasDown = buttons.guideButton;
 		Sleep(10);
 	}
 
